5ddde9701bc4d9a5911471b84b81ca29338981dd
pryvvid <pryvvid@gmail.com>
2019-03-18 19:11:56 +0300

    feat: find love triangles

--- src/index.js
+++ src/index.js
@@ -3,5 +3,44 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let numObj;
+  let checkedKeys = [];
+  let trianglesCount = 0;
+  let first, second, third;
+  if (preferences.length) numObj = makeObj(preferences);
+  if (numObj) {
+    for (let key in numObj) {
+      if (key == numObj[key]) continue;
+      if (checkNums(key, checkedKeys)) {
+      
+        first = numObj[key];
+        second = numObj[first];
+        third = numObj[second];
+        if (third == key) {
+          checkedKeys = [...checkedKeys, first, second];
+          trianglesCount++;
+        }
+      }
+    }
+  }
+  return trianglesCount;
 };
+
+function makeObj(arr) {
+  let obj = {};
+  if (arr.length) {
+    for (let i = 0; i < arr.length; i++) {
+      obj[i+1] = arr[i];
+    }
+  }
+  return obj;
+}
+
+function checkNums(num, arr) {
+  if (arr.length) {
+    for (let i of arr) {
+      if (i == num) return false;
+    }
+  }
+  return true;
+}
